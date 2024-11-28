7d311e6a237661846895a8f9fc51910021ecbbb5
Halleys123 <arnavadvik2003@gmail.com>
2024-11-29 02:29:00 +0530

    Add main function to compute maximum score from permutations of input string

--- 3110/One.cpp
+++ 3110/One.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int scoreOfString(string s)
 {
     int ans = 0;
@@ -6,4 +12,23 @@ int scoreOfString(string s)
         ans += abs(s[i] - s[i + 1]);
     }
     return ans;
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    vector<string> v;
+    sort(s.begin(), s.end());
+    do
+    {
+        v.push_back(s);
+    } while (next_permutation(s.begin(), s.end()));
+    int maxScore = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        maxScore = max(maxScore, scoreOfString(v[i]));
+    }
+    cout << maxScore << endl;
+    return 0;
 }
