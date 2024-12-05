4e9f8d1c9a804f9909f35ecfe4bd4d9c57e1ac65
Halleys123 <arnavadvik2003@gmail.com>
2024-12-05 23:23:00 +0530

    Implement 2610 Five.cpp

---
+++ 2610/Five.cpp
@@ -0,0 +1,49 @@
+// Single Loop
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> findMatrix(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+
+    for (int num : nums)
+    {
+        bool placed = false;
+        for (auto &row : ans)
+        {
+            if (row.back() != num)
+            {
+                row.push_back(num);
+                placed = true;
+                break;
+            }
+        }
+        if (!placed)
+        {
+            ans.push_back({num});
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 2, 3, 3, 3};
+    vector<vector<int>> ans = findMatrix(nums);
+    for (auto i : ans)
+    {
+        for (int j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
