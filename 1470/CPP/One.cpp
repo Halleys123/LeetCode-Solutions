d3d37074674160086441016bc70e73a51b851405
Halleys123 <arnavadvik2003@gmail.com>
2024-12-11 19:05:26 +0530

    1470 cpp complete

--- 1470/CPP/One.cpp
+++ 1470/CPP/One.cpp
@@ -1,3 +1,8 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 vector<int> shuffle(vector<int> &nums, int n)
 {
     vector<int> ans(2 * n, 0);
@@ -9,4 +14,16 @@ vector<int> shuffle(vector<int> &nums, int n)
         i += 2;
     }
     return ans;
+}
+
+int main()
+{
+    vector<int> nums = {2, 5, 1, 3, 4, 7};
+    int n = 3;
+    vector<int> ans = shuffle(nums, n);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
 }
