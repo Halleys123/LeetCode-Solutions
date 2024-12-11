4ae00f34f2033271267ecf3be8347462fde3ee4c
Halleys123 <arnavadvik2003@gmail.com>
2024-12-11 19:06:02 +0530

    removed std from 1470 cpp

--- 1470/CPP/One.cpp
+++ 1470/CPP/One.cpp
@@ -1,11 +1,9 @@
 #include <vector>
 #include <iostream>
 
-using namespace std;
-
-vector<int> shuffle(vector<int> &nums, int n)
+std::vector<int> shuffle(std::vector<int> &nums, int n)
 {
-    vector<int> ans(2 * n, 0);
+    std::vector<int> ans(2 * n, 0);
     int i = 0;
     while (i < 2 * n)
     {
@@ -18,12 +16,12 @@ vector<int> shuffle(vector<int> &nums, int n)
 
 int main()
 {
-    vector<int> nums = {2, 5, 1, 3, 4, 7};
+    std::vector<int> nums = {2, 5, 1, 3, 4, 7};
     int n = 3;
-    vector<int> ans = shuffle(nums, n);
+    std::vector<int> ans = shuffle(nums, n);
     for (int i = 0; i < ans.size(); i++)
     {
-        cout << ans[i] << " ";
+        std::cout << ans[i] << " ";
     }
     return 0;
 }
