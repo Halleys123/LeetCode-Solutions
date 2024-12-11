d60283827d8749ac24b5b695d3853338c4cd55be
Halleys123 <arnavadvik2003@gmail.com>
2024-12-11 19:21:50 +0530

    Completed 1470 three.cpp

--- 1470/CPP/Three.cpp
+++ 1470/CPP/Three.cpp
@@ -14,3 +14,15 @@ vector<int> shuffle(vector<int> &nums, int n)
     }
     return ans;
 }
+
+int main()
+{
+    vector<int> nums = {2, 5, 1, 3, 4, 7};
+    int n = 3;
+    vector<int> ans = shuffle(nums, n);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    return 0;
+}
